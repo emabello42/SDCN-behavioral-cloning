722aef8aacbb40235cf357ce6944e1516a141f59
emabello42 <emabello42@gmail.com>
2018-11-11 23:07:49 +0100

    Python generators for training and validation sets implemented

--- data_loader.py
+++ data_loader.py
@@ -1,6 +1,8 @@
 import csv
 import numpy as np
 from scipy import ndimage
+import sklearn
+from sklearn.model_selection import train_test_split
 
 class DataLoader(object):
     
@@ -42,3 +44,54 @@ class DataLoader(object):
         X_train = np.array(augmented_images)
         y_train = np.array(augmented_measurements)
         return X_train, y_train
+
+    def load_samples(self,test_size= 0.2, correction=0.2):
+        lines = []
+        with open('../data/driving_log.csv') as csvfile:
+            reader = csv.reader(csvfile)
+            for line in reader:
+                lines.append(line)
+
+        X_samples = []
+        y_measurements = []
+        for line in lines:
+            filename_img_center = line[0].split('/')[-1]
+            filename_img_left = line[1].split('/')[-1]
+            filename_img_right = line[2].split('/')[-1]
+            steering_center = float(line[3])
+            steering_left = steering_center + correction
+            steering_right = steering_center - correction
+            X_samples.append(filename_img_center)
+            X_samples.append(filename_img_left)
+            X_samples.append(filename_img_right)
+            y_measurements.append(steering_center)
+            y_measurements.append(steering_left)
+            y_measurements.append(steering_right)
+
+        X_samples = np.array(X_samples)
+        y_measurements = np.array(y_measurements)
+        X_train, X_valid, y_train, y_valid = train_test_split(X_samples, y_measurements, test_size=test_size, random_state=42)
+        return X_train, X_valid, y_train, y_valid
+    
+    def generator(self,X_samples, y_measurements, batch_size=128, path='../data/IMG/'):
+        num_samples = len(X_samples)
+        while True: # Loop forever so the generator never terminates
+            for offset in range(0, num_samples, batch_size):
+                batch_samples = X_samples[offset:offset+batch_size]
+                batch_measurements = y_measurements[offset:offset+batch_size]
+                images = []
+                angles = []
+                for i in range(0, len(batch_samples)):
+                    image = ndimage.imread(path + batch_samples[i])
+                    angle = batch_measurements[i]
+                    images.append(image)
+                    angles.append(angle)
+                    
+                    #augment the dataset with the corresponding flipped images and angles
+                    images.append(np.fliplr(image))
+                    angles.append(-angle)
+
+                # trim image to only see section with road
+                X_train = np.array(images)
+                y_train = np.array(angles)
+                yield sklearn.utils.shuffle(X_train, y_train)
--- model.py
+++ model.py
@@ -1,26 +1,39 @@
 from data_loader import DataLoader
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Cropping2D, Convolution2D
+from keras.layers import Flatten, Dense, Lambda, Cropping2D, Conv2D
+import pickle
 
 data_loader = DataLoader()
-X_train, y_train = data_loader.load()
+X_train, X_valid, y_train, y_valid = data_loader.load_samples()
+training_generator = data_loader.generator(X_train, y_train, batch_size=32)
+validation_generator = data_loader.generator(X_valid, y_valid, batch_size=32)
+#X_train, y_train = data_loader.load()
 
+print("Trainig samples:", len(X_train)*2) #original images + flipped images
+print("Validation samples:", len(X_valid)*2)#original images + flipped images
 model = Sequential()
 model.add(Lambda(lambda x: x/255.0 -0.5, input_shape=(160,320,3)))
 model.add(Cropping2D(cropping=((70,25), (0,0))))
-model.add(Convolution2D(24,5,5, subsample=(2,2), activation="relu"))
-model.add(Convolution2D(36,5,5, subsample=(2,2), activation="relu"))
-model.add(Convolution2D(48,5,5, subsample=(2,2), activation="relu"))
-model.add(Convolution2D(64,3,3, activation="relu"))
-model.add(Convolution2D(64,3,3, activation="relu"))
+model.add(Conv2D(24,(5,5), activation="relu", strides=(2,2)))
+model.add(Conv2D(36,(5,5), activation="relu", strides=(2,2)))
+model.add(Conv2D(48,(5,5), activation="relu", strides=(2,2)))
+model.add(Conv2D(64,(3,3), activation="relu"))
+model.add(Conv2D(64,(3,3), activation="relu"))
 model.add(Flatten())
 model.add(Dense(100))
 model.add(Dense(50))
 model.add(Dense(10))
 model.add(Dense(1))
 
-model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
-model.fit(X_train, y_train, validation_split=0.2, epochs=5, shuffle=True)
+model.compile(loss='mse', optimizer='adam')
+#model.fit(X_train, y_train, validation_split=0.2, epochs=5, shuffle=True)
 
-print(X_train.shape)
+history_object = model.fit_generator(training_generator, steps_per_epoch= \
+                    len(X_train), validation_data=validation_generator, \
+                    validation_steps=len(X_valid), epochs=5, verbose=1)
+
+
 model.save('model.h5')
+
+with open('loss.pkl', 'wb') as f:
+    pickle.dump(history_object, f, pickle.HIGHEST_PROTOCOL)
